2c2962dca6b2f61785af816f915d2fe4fd7c8fb2
CasperLindbergAtGithub <casperl@student.chalmers.se>
2020-12-14 20:00:43 +0100

    Added vocab

---
+++ Vocabulary.py
@@ -0,0 +1,64 @@
+from collections import Counter
+
+UNKNOWN = '<unk>'
+START = '<start>'
+END = '<end>'
+PAD = '<pad>'
+
+
+class Vocabulary:
+    """Manages the numerical encoding of the vocabulary."""
+
+    def __init__(self, min_token_freq=None, caption_max_len=50):
+        # String-to-integer mapping
+        self.stoi = None
+
+        # Minimal token frequency to be included in the vocabulary.
+        self.min_token_freq = min_token_freq
+
+        # Maximum allowed length of a caption
+        self.caption_max_len = caption_max_len
+
+    def build(self, captions):
+        """Builds the vocabulary, based on a set of captions."""
+
+        # Count token frequencies in captions
+        token_freqs = Counter(token for caption in captions for token in caption)
+
+        tokens = [token for token in token_freqs.keys() if token_freqs[token] > self.min_token_freq]
+
+        # Build the token mapping. Add special tokens to the vocabulary:
+        # <unk>, <start>, <end>, <pad>
+        self.stoi = {k: v + 1 for v, k in enumerate(tokens)}
+        self.stoi[UNKNOWN] = len(self.stoi) + 1
+        self.stoi[START] = len(self.stoi) + 1
+        self.stoi[END] = len(self.stoi) + 1
+        self.stoi[PAD] = 0
+
+    def encode(self, captions):
+        """Encodes a set of captions. Save caption lengths for a mask."""
+        enc = [[self.stoi[START]] +
+               [self.stoi.get(token, self.stoi[UNKNOWN]) for token in caption] +
+               [self.stoi[END]] +
+               [self.stoi[PAD]] * (self.caption_max_len - len(caption)) for caption in captions]
+        cap_len = [len(caption)+2 for caption in captions]
+        return enc, cap_len
+
+    def get_unknown_idx(self):
+        """Returns the integer index of the special dummy word representing unknown words."""
+        return self.stoi[UNKNOWN]
+
+    def get_pad_idx(self):
+        """Returns the integer index of the special padding dummy word."""
+        return self.stoi[PAD]
+
+    def get_start_idx(self):
+        """Returns the integer index of the special start dummy word."""
+        return self.stoi[START]
+
+    def get_end_idx(self):
+        """Returns the integer index of the special end dummy word."""
+        return self.stoi[END]
+
+    def __len__(self):
+        return len(self.stoi)
--- preprocess_data.py
+++ preprocess_data.py
@@ -2,7 +2,6 @@ import json
 from collections import Counter
 import os
 
-
 dataset_image_path = 'data/Flicker8k_Dataset'
 dataset_split_path = 'data/dataset_flickr8k.json'
 
@@ -20,29 +19,49 @@ def preprocess_data(caption_max_len: int = 50, min_word_freq: int = 5):
     captions_train = []
     captions_validation = []
     captions_test = []
-    word_freq = Counter()
 
     with open(dataset_split_path, 'r') as f:
         dataset = json.load(f)
 
     for image in dataset["images"]:
-        captions = process_captions(caption_max_len, image, word_freq)
+        captions = process_captions(caption_max_len, image)
 
         if len(captions) == 0:
             continue
 
         path = os.path.join(dataset_image_path, image['filename'])
 
-        
+        train_val_test_split(captions, captions_test, captions_train, captions_validation, image, image_test,
+                             image_train, image_validation, path)
+
+        assert_valid_train_val_test_split(captions_test, captions_train, captions_validation, image_test, image_train,
+                                          image_validation)
 
 
-def process_captions(caption_max_len, image, word_freq):
+def train_val_test_split(captions, captions_test, captions_train, captions_validation, image, image_test, image_train,
+                         image_validation, path):
+    if image['split'] in {'train'}:
+        image_train.append(path)
+        captions_train.append(captions)
+    elif image['split'] in {'val'}:
+        image_validation.append(path)
+        captions_validation.append(captions)
+    elif image['split'] in {'test'}:
+        image_test.append(path)
+        captions_test.append(captions)
+
+
+def assert_valid_train_val_test_split(captions_test, captions_train, captions_validation, image_test, image_train,
+                                      image_validation):
+    assert len(image_train) == len(captions_train)
+    assert len(image_validation) == len(captions_validation)
+    assert len(image_test) == len(captions_test)
+
+
+def process_captions(caption_max_len, image):
     captions = []
     for caption in image['sentences']:
         tokens = caption["tokens"]
-        word_freq.update(tokens)
         if len(tokens) <= caption_max_len:
             captions.append(tokens)
     return captions
-
-
